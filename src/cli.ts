18628ad86daab9f8530688a73d87e69a819c3fa0
Lily Skye <git@suchipi.tv>
2026-03-09 12:25:47 -0600

    chalk -> kleur

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import type { Result } from "./types";
-import chalk from "chalk";
+import kleur from "kleur";
 import * as babelCodeFrame from "@babel/code-frame";
 import ora from "ora";
 import grepAst from "./index";
@@ -14,12 +14,12 @@ import defaults from "./defaults";
     const options = parseArgv(argv);
 
     const patternsSpinner = ora(
-      chalk.blue(
+      kleur.blue(
         `Matching patterns: ${JSON.stringify(
           options.patterns || defaults.patterns
         )}`
       ) +
-        chalk.grey(
+        kleur.grey(
           ` (gitignore: ${
             options.gitignore || defaults.gitignore ? "enabled" : "disabled"
           })`
@@ -31,16 +31,16 @@ import defaults from "./defaults";
     const results: Array<Result> = await grepAst(
       argv,
       (files: Array<string>) => {
-        patternsSpinner.succeed(chalk.green(`Found ${files.length} files.`));
-        filesSpinner.start(chalk.blue("Parsing files..."));
+        patternsSpinner.succeed(kleur.green(`Found ${files.length} files.`));
+        filesSpinner.start(kleur.blue("Parsing files..."));
       }
     );
-    filesSpinner.succeed(chalk.green("Parsed files."));
+    filesSpinner.succeed(kleur.green("Parsed files."));
 
-    process.stderr.write("\n" + chalk.blue("Results:") + "\n");
+    process.stderr.write("\n" + kleur.blue("Results:") + "\n");
     results.forEach((result) => {
       if (result.error) {
-        process.stderr.write(chalk.red(result.message) + "\n");
+        process.stderr.write(kleur.red(result.message) + "\n");
         process.exitCode = 1;
       } else {
         const codeFrame = babelCodeFrame.codeFrameColumns(
@@ -62,7 +62,7 @@ import defaults from "./defaults";
         );
         process.stderr.write(
           result.filepath +
-            chalk.grey(
+            kleur.grey(
               `:${result.loc.start.line}:${result.loc.start.column}-${result.loc.end.line}:${result.loc.end.column}`
             ) +
             "\n"
@@ -74,27 +74,27 @@ import defaults from "./defaults";
 
     const nonErrorResults = results.filter((result) => !result.error);
     process.stderr.write(
-      "\n" + chalk.blue(`${nonErrorResults.length} total matches:`) + "\n"
+      "\n" + kleur.blue(`${nonErrorResults.length} total matches:`) + "\n"
     );
     nonErrorResults.forEach((result) => {
       if (result.error) return; // for type narrowing
       process.stdout.write(
         result.filepath +
-          chalk.grey(
+          kleur.grey(
             `:${result.loc.start.line}:${result.loc.start.column}-${result.loc.end.line}:${result.loc.end.column}`
           ) +
           "\n"
       );
     });
   } catch (error) {
-    process.stderr.write(chalk.red(error) + "\n");
+    process.stderr.write(kleur.red(String(error)) + "\n");
     if (
       typeof error === "object" &&
       error != null &&
       "stack" in error &&
       typeof error.stack === "string"
     ) {
-      process.stderr.write(chalk.red(error.stack) + "\n");
+      process.stderr.write(kleur.red(error.stack) + "\n");
     }
     process.exit(1);
   }
